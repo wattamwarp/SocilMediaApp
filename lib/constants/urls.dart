b329c307d056a97ceee8e3ef2b1d0fcd52db10ca
Padmanabh Wattamwar <wattamwarp874@gmail.com>
2021-07-07 13:43:53 +0530

    api key

--- lib/constants/urls.dart
+++ lib/constants/urls.dart
@@ -1,6 +1,6 @@
 class Urls{
   Urls();
-  static final mainUrl="https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=42fc031a2f856abb6ec859629b989089&tags=food&text=food&safe_search=3&per_page=10&format=json&nojsoncallback=1&auth_token=72157719556549080-eebd9fcd6d6b7545&api_sig=dab5b258e17abc5581dac968390aaad4";
+  static final mainUrl="https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=039013f8d20af5839fc63e100d2b4cba&tags=food&text=food&per_page=10&format=json&nojsoncallback=1&auth_token=72157719561461640-77b82207155fbb8c&api_sig=009867d7115948b498c92d6237336d61";
   static final accountImgUrl='https://media.istockphoto.com/photos/portrait-of-smiling-handsome-man-in-blue-tshirt-standing-with-crossed-picture-id1045886560?k=6&m=1045886560&s=612x612&w=0&h=hXrxai1QKrfdqWdORI4TZ-M0ceCVakt4o6532vHaS3I=';
   static final postImgUrl='https://www.thestatesman.com/wp-content/uploads/2019/12/fruitseller.jpg';
 
